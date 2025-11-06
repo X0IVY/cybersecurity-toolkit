e7c93e325f367d2283c5579e4d3d9c8ec2a74fa7
Luis Antonio Marroquin Salazar <lmarroquin4567@gmail.com>
2025-11-06 13:45:38 -0800

    cleaned up the code, removed extra stuff

--- hash_tools.py
+++ hash_tools.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python3
-"""
-Hash Tools - Simple hash generator for cybersecurity toolkit
-Supports: MD5, SHA-1, SHA-256, SHA-512
-"""
+# quick hash tool i made for the toolkit
+# supports md5, sha1, sha256, sha512
 
 import hashlib
-from colorama import Fore, Style, init
+from colorama import Fore, init
 
-# Initialize colorama
 init(autoreset=True)
 
 def generate_hash(text, hash_type='sha256'):
-    """
-    Generate hash from text string
-    
-    Args:
-        text (str): Text to hash
-        hash_type (str): Type of hash (md5, sha1, sha256, sha512)
-    
-    Returns:
-        str: Hex digest of the hash
-    """
     hash_type = hash_type.lower()
     
     if hash_type == 'md5':
@@ -32,98 +19,80 @@ def generate_hash(text, hash_type='sha256'):
     elif hash_type == 'sha512':
         return hashlib.sha512(text.encode()).hexdigest()
     else:
-        raise ValueError(f"Unsupported hash type: {hash_type}")
-
-def hash_file(filepath, hash_type='sha256'):
-    """
-    Generate hash from file
-    
-    Args:
-        filepath (str): Path to file
-        hash_type (str): Type of hash
-    
-    Returns:
-        str: Hex digest of the hash
-    """
-    hash_type = hash_type.lower()
-    
-    # Create hash object
-    if hash_type == 'md5':
-        hash_obj = hashlib.md5()
-    elif hash_type == 'sha1':
-        hash_obj = hashlib.sha1()
-    elif hash_type == 'sha256':
-        hash_obj = hashlib.sha256()
-    elif hash_type == 'sha512':
-        hash_obj = hashlib.sha512()
-    else:
-        raise ValueError(f"Unsupported hash type: {hash_type}")
-    
-    # Read file and update hash
-    try:
-        with open(filepath, 'rb') as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_obj.update(chunk)
-        return hash_obj.hexdigest()
-    except FileNotFoundError:
         return None
 
-def print_banner():
-    """Print tool banner"""
-    print(Fore.CYAN + "\n" + "="*50)
-    print(Fore.CYAN + "    HASH GENERATOR - Cybersecurity Toolkit")
-    print(Fore.CYAN + "="*50 + "\n")
-
-def print_hash_result(hash_type, hash_value):
-    """Print hash result with formatting"""
-    print(Fore.GREEN + f"\n{hash_type.upper()} Hash:")
-    print(Fore.YELLOW + hash_value)
-    print()
+def hash_file(filepath, hash_type='sha256'):
+    hash_type = hash_type.lower()
+    
+    if hash_type == 'md5':
+        h = hashlib.md5()
+    elif hash_type == 'sha1':
+        h = hashlib.sha1()
+    elif hash_type == 'sha256':
+        h = hashlib.sha256()
+    elif hash_type == 'sha512':
+        h = hashlib.sha512()
+    else:
+        return None
+    
+    try:
+        with open(filepath, 'rb') as f:
+            while True:
+                data = f.read(4096)
+                if not data:
+                    break
+                h.update(data)
+        return h.hexdigest()
+    except:
+        return None
 
 if __name__ == "__main__":
-    print_banner()
+    print(Fore.CYAN + "\n" + "="*50)
+    print(Fore.CYAN + "    Hash Generator")
+    print(Fore.CYAN + "="*50 + "\n")
     
-    print(Fore.WHITE + "Choose an option:")
-    print("1. Hash text string")
+    print("What do you want to do?")
+    print("1. Hash text")
     print("2. Hash file")
     print("3. Exit")
     
-    choice = input(Fore.WHITE + "\nEnter choice (1-3): ")
+    choice = input("\nChoice: ")
     
     if choice == '1':
-        text = input(Fore.WHITE + "\nEnter text to hash: ")
-        print(Fore.WHITE + "\nSelect hash type:")
-        print("1. MD5")
+        text = input("\nText to hash: ")
+        print("\nHash type:")
+        print("1. MD5 (not secure)")
         print("2. SHA-1")
         print("3. SHA-256")
         print("4. SHA-512")
         
-        hash_choice = input(Fore.WHITE + "\nEnter choice (1-4): ")
+        h = input("\nPick one: ")
+        types = {'1': 'md5', '2': 'sha1', '3': 'sha256', '4': 'sha512'}
+        hash_type = types.get(h, 'sha256')
         
-        hash_types = {'1': 'md5', '2': 'sha1', '3': 'sha256', '4': 'sha512'}
-        hash_type = hash_types.get(hash_choice, 'sha256')
-        
-        hash_value = generate_hash(text, hash_type)
-        print_hash_result(hash_type, hash_value)
+        result = generate_hash(text, hash_type)
+        if result:
+            print(Fore.GREEN + f"\n{hash_type.upper()}:")
+            print(Fore.YELLOW + result + "\n")
     
     elif choice == '2':
-        filepath = input(Fore.WHITE + "\nEnter file path: ")
-        print(Fore.WHITE + "\nSelect hash type:")
+        filepath = input("\nFile path: ")
+        print("\nHash type:")
         print("1. MD5")
         print("2. SHA-1")
         print("3. SHA-256")
         print("4. SHA-512")
         
-        hash_choice = input(Fore.WHITE + "\nEnter choice (1-4): ")
+        h = input("\nPick one: ")
+        types = {'1': 'md5', '2': 'sha1', '3': 'sha256', '4': 'sha512'}
+        hash_type = types.get(h, 'sha256')
         
-        hash_types = {'1': 'md5', '2': 'sha1', '3': 'sha256', '4': 'sha512'}
-        hash_type = hash_types.get(hash_choice, 'sha256')
-        
-        hash_value = hash_file(filepath, hash_type)
-        if hash_value:
-            print_hash_result(hash_type, hash_value)
+        result = hash_file(filepath, hash_type)
+        if result:
+            print(Fore.GREEN + f"\n{hash_type.upper()}:")
+            print(Fore.YELLOW + result + "\n")
         else:
-            print(Fore.RED + "\nError: File not found!")
+            print(Fore.RED + "\nFile not found or error\n")
     
     else:
-        print(Fore.WHITE + "\nExiting...\n")
+        print("\nBye\n")
